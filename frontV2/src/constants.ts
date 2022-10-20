ba6418c4cd7b5633975e28dc3796fb0fca3d4357
Fred <iblueman260@gmail.com>
2022-10-20 23:11:09 +0300

    feat(project_refactor): Switch development port to 900*, fix build CI error

--- frontV2/src/constants.ts
+++ frontV2/src/constants.ts
@@ -3,4 +3,4 @@ export const NEW_USER_URL = "accounts";
 export const noNumbersRegex = /^([^0-9]*)$/;
 export const onlyNumbersRegex = /^(0|[1-9][0-9]*)$/;
 
-export const BACK_URL = "http://localhost:8880"
+export const BACK_URL = "http://localhost:9001"
